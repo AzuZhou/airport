dfcbef4cf019e3b5afdf7fcb1a65c0f9372f23f1
Azu <abbey-1825@hotmail.com>
2018-03-14 17:55:22 -0300

    Making the class Scheduler contain its own info

--- airport/index.js
+++ airport/index.js
@@ -1,41 +1,17 @@
 const Scheduler = require('./scheduler.js')
 
-let dateTime, newFlight, scheduledFlights, closestFlight, arr, closestFlightDateTime, newFlightId
+let newFlight
 
-dateTime = "October 13, 2014 11:36:00"
-
-scheduledFlights = [{
+newFlight = new Scheduler([{
     id: 0,
-    dateTime: "October 13, 2014 11:15:00"
+    dateTime: "October 13, 2014 11:55:00"
 },
 {
     id: 1,
-    dateTime: "October 13, 2014 11:16:00"
-}] //array of each scheduled flight with id and dateTime
-
-arr = (scheduledFlights.map(x => Date.parse(x.dateTime))).map(y => Math.abs(y - Date.parse(dateTime))) //array with the difference in time from current flight aka newFlight
-
-closestFlight = arr.reduce( //gets the closest flight
-    (accumulator, currentValue) => {
-        return accumulator < currentValue ? accumulator : currentValue
-    }
-)
-
-closestFlightDateTime = scheduledFlights.filter(function (flight) { //gets the actual dateTime of the closest flight
-    return flight.id === arr.indexOf(closestFlight)
-})[0].dateTime
-
-newFlight = new Scheduler(dateTime, closestFlightDateTime)
-
-if (newFlight.CouldScheduleAt()) {
-    newFlightId = scheduledFlights.length
-    scheduledFlights.push({
-        id: newFlightId,
-        dateTime: dateTime
-    })
-}
+    dateTime: "October 13, 2014 12:56:00"
+}])
 
 
-
-
+console.log(newFlight.unscheduleAt("October 13, 2014 12:56:00"))
+console.log(newFlight.flights)
 
--- airport/scheduler.js
+++ airport/scheduler.js
@@ -1,25 +1,47 @@
 module.exports = class Scheduler {
-    constructor(dateTime, closestFlightDateTime) { //takes in date string
-        this.dateTime = dateTime
-        this.closestFlightDateTime = closestFlightDateTime
+    constructor(initialFlights = []) {
+        this.flights = initialFlights
     }
 
-    CouldScheduleAt() { // returns true if there’s room to schedule at ‘time’
-        return (Math.abs(Date.parse(this.closestFlightDateTime) - Date.parse(this.dateTime)) > 1200000) ? true : false
+    closestFlightDateTime(dateTime) {
+        let arr = (this.flights.map(x => Date.parse(x.dateTime))).map(y => Math.abs(y - Date.parse(dateTime))) //array with the difference in time from current flight aka newFlight
+        let closestFlight = arr.reduce( //gets the closest flight
+            (accumulator, currentValue) => {
+                return accumulator < currentValue ? accumulator : currentValue
+            }
+        )
+        return this.flights.filter(flight => flight.id === arr.indexOf(closestFlight))[0].dateTime //gets the actual dateTime of the closest flight
     }
 
-    ScheduleAt() { // returns true if we successfully scheduled
-        return this.CouldScheduleAt() ? true : false
+    findAvailableDateTime(dateTime) { // Choose an available time to schedule at, and return that time
+        return (new Date(Date.parse(this.closestFlightDateTime(dateTime)) + 1200000)).toString() //get a possible dateTime to schedule, in this case the closest possible
     }
 
-    Schedule() { // Choose an available time to schedule at, and return that time
-        if (!this.ScheduleAt()) { //if the dateTime was not available, find one that is
-            return (new Date(Date.parse(this.closestFlightDateTime) + 1200000)).toString()
+    couldScheduleAt(dateTime) { // returns true if there’s room to schedule at ‘time’
+        return Boolean(Math.abs(Date.parse(this.closestFlightDateTime(dateTime)) - Date.parse(dateTime)) > 1200000)
+    }
+
+    scheduleAt(dateTime) { //this ckecks if there is a flight scheduled for dateTime
+        return Boolean(this.flights.filter(flight => flight.dateTime === dateTime)[0]) // returns true if we successfully scheduled
+    }
+
+    schedule(dateTime) {
+        let newFlightId
+        if (this.couldScheduleAt(dateTime)) { //if couldScheduleAt returns true, schedule that flight and return true. Otherwise, get return the next possible flight dateTime
+            newFlightId = this.flights.length
+            this.flights.push({
+                id: newFlightId,
+                dateTime: dateTime
+            })
+            return this.scheduleAt(dateTime)
+        } else {
+            return this.findAvailableDateTime(dateTime)
         }
     }
 
-    UnscheduleAt() { // returns true if we successfully unscheduled something
-
+    unscheduleAt(dateTime) { //deletes flight (even if it doesn't exist) and returns true if there in nothing scheduled for this dateTime
+        this.flights = this.flights.filter(flight => flight.dateTime !== dateTime)
+        return Boolean(this.flights.filter(flight => flight.dateTime === dateTime).length === 0)
     }
 }
 
